a5021df7e4c55c9ccd4728f1b9943a7fed1b3368
stepheweffie <stephie.maths@gmail.com>
2023-09-30 18:53:55 -0400

    run sort_results.py for visuals

--- sort_results.py
+++ sort_results.py
@@ -1,4 +1,6 @@
 from pandas import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 syntactic_csv = 'syntactic_results.csv'
 
 
@@ -27,10 +29,52 @@ def sorted_syntactic(csv_file):
         group_data = top_74[top_74['Wolfe URL Video 2'] == group]
         print(f"\nTop 10% Statistics For Video: {group}\n{'-' * 40}")
         print(group_data.describe())
-    return group_74
+    return top_74, non_zero_counts
+
+
+def syntactic_visuals():
+    top_74, non_zero_counts = sorted_syntactic(syntactic_csv)
+    # Ensure the plots are displayed inline in the Jupyter Notebook (if that's what you're using
+
+    # Set a default style
+    sns.set_style("whitegrid")
+
+    # 1. Bar Plot of Group Counts
+    plt.figure(figsize=(10, 6))
+    non_zero_counts.plot(kind='bar')
+    plt.title("Videos Occur in Top 74 Records")
+    plt.ylabel("Count")
+    plt.xlabel("Wolfe Video")
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show()
+
+    # 2. Box Plots for Similarity Scores
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(data=top_74, x='Wolfe URL Video 2', y='Similarity Score', order=non_zero_counts.index)
+    plt.title("Distribution of Similarity Scores by Video")
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show()
+
+    # 3. Histogram of Similarity Scores
+    plt.figure(figsize=(10, 6))
+    for group in non_zero_counts.index[:10]:
+        group_data = top_74[top_74['Wolfe URL Video 2'] == group]
+        # Select only the first 10 records for this group
+        first_10 = group_data.head(10)
+        sns.histplot(first_10['Similarity Score'], label=group, kde=True)
+    plt.title("Distribution of Similarity Scores for First 10 of the 15 Videos")
+    plt.xlabel("Similarity Score")
+    plt.ylabel("Frequency")
+    plt.legend()
+
+    # Adjusting the x-axis limits to focus on the region between 0.8 and 1.0
+    plt.xlim(0.899, .94)
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
-    sorted_syntactic(syntactic_csv)
-
+    syntactic_visuals()
 
